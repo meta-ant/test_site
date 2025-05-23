bbc53f14631afc6dba9f3ff646c56566468dd843
meta-ant <anthony@metarouter.io>
2025-05-23 15:55:17 -0400

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,36 +1,18 @@
-!(function () {
+(function() {
   var analytics = (window.analytics = window.analytics || []);
   if (!analytics.initialize) {
     if (analytics.invoked) {
-      window.console &&
-        console.error &&
-        console.error("Analytics.js snippet included twice.");
+      console.error("Analytics.js snippet included twice.");
     } else {
       analytics.invoked = true;
       analytics.methods = [
-        "trackSubmit",
-        "trackClick",
-        "trackLink",
-        "trackForm",
-        "pageview",
-        "identify",
-        "reset",
-        "group",
-        "track",
-        "ready",
-        "alias",
-        "debug",
-        "page",
-        "once",
-        "off",
-        "on",
-        "addSourceMiddleware",
-        "addIntegrationMiddleware",
-        "setAnonymousId",
-        "addDestinationMiddleware",
+        "trackSubmit", "trackClick", "trackLink", "trackForm", "pageview",
+        "identify", "reset", "group", "track", "ready", "alias", "debug",
+        "page", "once", "off", "on", "addSourceMiddleware",
+        "addIntegrationMiddleware", "setAnonymousId", "addDestinationMiddleware"
       ];
-      analytics.factory = function (method) {
-        return function () {
+      analytics.factory = function(method) {
+        return function() {
           var args = Array.prototype.slice.call(arguments);
           args.unshift(method);
           analytics.push(args);
@@ -38,19 +20,22 @@
         };
       };
       for (var i = 0; i < analytics.methods.length; i++) {
-        var key = analytics.methods[i];
-        analytics[key] = analytics.factory(key);
+        analytics[analytics.methods[i]] = analytics.factory(analytics.methods[i]);
       }
-      analytics.load = function (writeKey, options) {
+
+      analytics.load = function(writeKey, options) {
         var script = document.createElement("script");
         script.type = "text/javascript";
         script.async = true;
-        script.src = "https://replaceme.com/" + writeKey + ".js"; // CHANGE THIS
+        script.src = "https://ajs.metarouter.io/demo_anthony.js";
         var first = document.getElementsByTagName("script")[0];
         first.parentNode.insertBefore(script, first);
         analytics._loadOptions = options;
       };
+
       analytics.SNIPPET_VERSION = "4.13.1";
+      analytics.load("demo_anthony");
+      analytics.page();
     }
   }
 })();
