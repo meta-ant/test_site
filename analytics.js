0c7a61347346bed53eb8ddb326ba22a8588f0ed9
meta-ant <anthony@metarouter.io>
2025-05-23 16:01:03 -0400

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,41 +1,60 @@
-(function() {
+<script type='text/javascript'>
+!(function() {
   var analytics = (window.analytics = window.analytics || []);
   if (!analytics.initialize) {
     if (analytics.invoked) {
-      console.error("Analytics.js snippet included twice.");
+      window.console &&
+        console.error &&
+        console.error('Analytics.js snippet included twice.');
     } else {
-      analytics.invoked = true;
+      analytics.invoked = !0;
       analytics.methods = [
-        "trackSubmit", "trackClick", "trackLink", "trackForm", "pageview",
-        "identify", "reset", "group", "track", "ready", "alias", "debug",
-        "page", "once", "off", "on", "addSourceMiddleware",
-        "addIntegrationMiddleware", "setAnonymousId", "addDestinationMiddleware"
+        'trackSubmit',
+        'trackClick',
+        'trackLink',
+        'trackForm',
+        'pageview',
+        'identify',
+        'reset',
+        'group',
+        'track',
+        'ready',
+        'alias',
+        'debug',
+        'page',
+        'once',
+        'off',
+        'on',
+        'addSourceMiddleware',
+        'addIntegrationMiddleware',
+        'setAnonymousId',
+        'addDestinationMiddleware'
       ];
-      analytics.factory = function(method) {
+      analytics.factory = function(e) {
         return function() {
-          var args = Array.prototype.slice.call(arguments);
-          args.unshift(method);
-          analytics.push(args);
+          var t = Array.prototype.slice.call(arguments);
+          t.unshift(e);
+          analytics.push(t);
           return analytics;
         };
       };
-      for (var i = 0; i < analytics.methods.length; i++) {
-        analytics[analytics.methods[i]] = analytics.factory(analytics.methods[i]);
+      for (var e = 0; e < analytics.methods.length; e++) {
+        var key = analytics.methods[e];
+        analytics[key] = analytics.factory(key);
       }
-
-      analytics.load = function(writeKey, options) {
-        var script = document.createElement("script");
-        script.type = "text/javascript";
-        script.async = true;
-        script.src = "https://ajs.metarouter.io/demo_anthony.js";
-        var first = document.getElementsByTagName("script")[0];
-        first.parentNode.insertBefore(script, first);
-        analytics._loadOptions = options;
+      analytics.load = function(key, e) {
+        var t = document.createElement('script');
+        t.type = 'text/javascript';
+        t.async = !0;
+        t.src = "https://meta-ant.github.io/test_site/analytics.js";
+        var n = document.getElementsByTagName('script')[0];
+        n.parentNode.insertBefore(t, n);
+        analytics._loadOptions = e;
       };
-
-      analytics.SNIPPET_VERSION = "4.13.1";
+      analytics.SNIPPET_VERSION = '4.13.1';
       analytics.load("demo_anthony");
-      analytics.page();
+      window.analytics.page();
     }
   }
 })();
+</script>
